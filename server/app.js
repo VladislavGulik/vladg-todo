ad812f6a38147a1b38b232d1b6b54369cca3d28a
Vladislav Gulik <hulik.uladzislau@gmail.com>
2017-12-28 16:03:31 +0300

    Composed: user module; #25

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const cors = require('cors');
 const passport = require('passport-jwt');
 const app = express();
@@ -52,14 +52,14 @@ app.set('view engine', 'jade');
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
